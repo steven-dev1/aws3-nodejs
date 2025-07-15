9bb9a8561bbc8fa6790a4d5ca560764b2d2f2c60
Steven Gonzalez <steven0810ange@gmail.com>
2025-07-15 18:29:27 -0500

    project setup finished

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const app = express()
 const PORT = 3000
 
 app.use(express.json())
-app.use(fileUpload())
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads"
+}))
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
+})
+
+app.post("/files", (req, res) => {
+    console.log(req.files)
+    res.send("File uploaded successfully!")
 })
 
 app.listen(PORT, () => {
