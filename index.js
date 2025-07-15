15956d79aea1d114c9cd81e129e47e6b8f3eb747
Steven Gonzalez <steven0810ange@gmail.com>
2025-07-15 18:19:26 -0500

    project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import fileUpload from "express-fileupload"
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(fileUpload())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
